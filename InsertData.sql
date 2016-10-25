eb95caa6c2928883438fc304c64c2ee26bef57e3
David Mazmanian <davidmazmanian@hotmail.com>
2016-10-25 11:14:35 -0400

    Insert data

---
+++ InsertData.sql
@@ -0,0 +1,7 @@
+INSERT INTO dbo.Products (ProductID, ProductName, Price, ProductDescription)
+	VALUES
+		(1, 'Clamp', 12.48, 'Workbench clamp'),
+		(50, 'Screwdriver', 3.17, 'Flat head'),
+		(75, 'Tire Bar', NULL, 'Tool for changing tires'),
+		(3000, '3mm Bracket', .52, NULL),
+GO
